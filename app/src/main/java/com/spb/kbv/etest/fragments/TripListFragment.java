a02529ad1441e6af9d46ee3b8deb665f2b68093b
klimovavera <bogdan.zp@mail.ru>
2016-08-02 00:55:22 +0300

    Second Commit

--- app/src/main/java/com/spb/kbv/etest/fragments/TripListFragment.java
+++ app/src/main/java/com/spb/kbv/etest/fragments/TripListFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -46,7 +45,7 @@ public class TripListFragment extends Fragment {
             @Override
             public void onResponse(Call<List<Trip>> call, Response<List<Trip>> response) {
                 mTripList.addAll(response.body());
-                Collections.sort(mTripList);
+                Collections.sort(mTripList, Collections.<Trip>reverseOrder());
                 mAdapter.notifyDataSetChanged();
                 showEmptyData(false);
             }
